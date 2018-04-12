74e347921d7409248d3f178fa4385f188e2522a5
BenjaminMorrell <benjamin.morrell1@gmail.com>
2018-04-12 21:24:59 +1000

    unpacking message

--- voxblox_ros/python/esdf_listenerpy.py
+++ voxblox_ros/python/esdf_listenerpy.py
@@ -8,6 +8,18 @@ def callback(data):
 
   rospy.loginfo(rospy.get_caller_id() + "In callback from esdf listening in python")
 
+  # Try to pull out parts of the message
+  rospy.loginfo("Voxel size is: %f, and there are %d voxels per size", data.voxel_size,data.voxels_per_side)
+
+  rospy.loginfo("Layer type is: %s",data.layer_type)
+
+  rospy.loginfo("action is: %d",data.action)
+
+  rospy.loginfo("Number of blocks: %d", len(data.blocks))
+
+  esdf_layer = voxblox.EsdfLayer(data.voxel_size,
+                                                data.voxels_per_side)
+
   # voxblox.EsdfMap
   # self.esdf_layer = voxblox.loadEsdfLayer(filename)
   # self.global_dict['fsp_out_map'] = voxblox.EsdfMap(self.esdf_layer)
